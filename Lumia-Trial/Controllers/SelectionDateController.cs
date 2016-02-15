b80a32f3c492b2131fd1689a0d54def82387d360
acarrington <asa.carrington@kpmg.co.uk>
2016-02-15 08:34:14 +0000

    Added Refresh Date Selection

---
+++ Lumia-Trial/Controllers/SelectionDateController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lumia_Trial.Models;
+using Lumia_Trial.Services.Interfaces;
+
+namespace Lumia_Trial.Controllers
+{
+    public class SelectionDateController : Controller
+    {
+        private readonly IRegionService regionService;
+        private readonly ISelectionDateService selectionDateService;
+        private readonly ILumiaTrialFormDataService lumiaTrialFormDataService;
+
+
+        public SelectionDateController(IRegionService regionService, ISelectionDateService selectionDateService, ILumiaTrialFormDataService lumiaTrialFormDataService)
+        {
+            this.regionService = regionService;
+            this.selectionDateService = selectionDateService;
+            this.lumiaTrialFormDataService = lumiaTrialFormDataService;
+        }
+
+        public ActionResult Index(Guid regionId)
+        {
+            var model = new SelectionDateViewModel();
+            this.BuildViewModel(model, regionId);
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Index(SelectionDateViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            return View();
+        }
+
+        private void BuildViewModel(SelectionDateViewModel model, Guid regionId)
+        {
+            model.SelectionDates = this.selectionDateService.GetAll(regionId).ToList();
+        }
+    }
+}
---
+++ Lumia-Trial/Models/SelectionDateViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Lumia_Trial.DataAccess.DataEntites;
+
+namespace Lumia_Trial.Models
+{
+    public class SelectionDateViewModel
+    {
+        public Guid SelectionDateId { get; set; }
+        public List<SelectionDate> SelectionDates { get; set; }
+    }
+}
