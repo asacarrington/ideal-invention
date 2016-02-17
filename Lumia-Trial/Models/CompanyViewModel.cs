07cfde810335562b8c42b37231b486cb8ef04773
acarrington <asa.carrington@kpmg.co.uk>
2016-02-17 08:59:16 +0000

    Passed Region Guid to company controller

--- Lumia-Trial/Models/CompanyViewModel.cs
+++ Lumia-Trial/Models/CompanyViewModel.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Data.Entity.Migrations.Model;
 using System.Linq;
 using System.Web;
+using System.Web.UI.WebControls;
 using Lumia_Trial.DataAccess.DataEntites;
 using Lumia_Trial.DataAccess.entites;
 
@@ -21,39 +23,54 @@ namespace Lumia_Trial.Models
         public List<Device> Devices { get; set; }
 
         [Required]
+        [DisplayName("Contact Name")]
         public string ContactName { get; set; }
 
         [Required]
+        [DisplayName("Email Address")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "E-mail is not valid")]
         public string EmailAddress { get; set; }
 
         [Required]
+        [DisplayName("Address")]
         public string Address { get; set; }
 
         [Required]
         public string Number { get; set; }
 
         [Required]
+        [DisplayName("Postcode")]
         public string PostCode { get; set; }
 
         [Required]
+        [DisplayName("Job Title")]
         public string JobTitle  { get; set; }
 
         [Required]
+        [DisplayName("Company Name")]
         public string CompanyName { get; set; }
+
+        [DisplayName("Handset Opotunities")]
 
         public List<HandsetOpotunity> HandsetOpotunities { get; set; }
 
+        [DisplayName("Handset Opotunities")]
         public Guid HandsetOpotunitiesId { get; set; }
-
+  
+        [DisplayName("Handset Refresh Date")]
         public string HandsetRefreshDate { get; set; }
 
+        [DisplayName("Current Handset Provider")]
         public string CurrentHandsetProvider { get; set; }
 
+        [DisplayName("Currenct Handset Operator")]
         public string CurrentHandsetOperator { get; set; }
         
         [Required]
+        [DisplayName("Accept Terms")]
         public bool IsTermsAcepted { get; set; }
 
+        [DisplayName("Allow Data To Be Shared")]
         public bool IsSharedInformationAllowed { get; set; }
 
         public TimeFrame TimeFrame { get; set; }
